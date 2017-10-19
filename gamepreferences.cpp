28386c8dabce1940b915a89ec6ffd964f3ae5440
Lukasz Smieszkowicz <l.smieszkowicz@gmail.com>
2017-10-19 18:23:21 +0200

    Initial commit

---
+++ gamepreferences.cpp
@@ -0,0 +1,6 @@
+#include "gamepreferences.h"
+
+GamePreferences::GamePreferences()
+{
+
+}
---
+++ gamepreferences.h
@@ -0,0 +1,11 @@
+#ifndef GAMEPREFERENCES_H
+#define GAMEPREFERENCES_H
+
+
+class GamePreferences
+{
+public:
+    GamePreferences();
+};
+
+#endif // GAMEPREFERENCES_H
